31d133ce821413eb9296338247796abb9e74441a
yamakon <askkyodai@gmail.com>
2021-12-06 18:41:41 +0900

    Adjust Navbar

--- components/Layout.js
+++ components/Layout.js
@@ -9,7 +9,27 @@ export default function Layout({ children, title = "HP by Nextjs"}) {
                 <title>{title}</title>
             </head>
             <header>
-                <nav className="bg-green-800 w-screen"></nav>
+                <nav className="bg-green-800 w-screen">
+                    <div flex items-center pl-8 h-14>
+                        <div className="flex space-x-4">
+                            <Link href="/">
+                                <a className="text-yellow-100 hover:bg-green-500 px-3 py-2 rounded">
+                                    Home
+                                </a>
+                            </Link>
+                            <Link href="/blog-page">
+                                <a className="text-yellow-100 hover:bg-green-500 px-3 py-2 rounded">
+                                    Blog
+                                </a>
+                            </Link>
+                            <Link href="/contact-page">
+                                <a className="text-yellow-100 hover:bg-green-500 px-3 py-2 rounded">
+                                    Contact
+                                </a>
+                            </Link>
+                        </div>
+                    </div>
+                </nav>
             </header>
             <main className="flex flex-1 justify-center items-center flex-col w-screen">
                 {children}
